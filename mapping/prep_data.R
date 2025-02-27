2e1a1657d478060a3fcf28dc5d47c851429bd91a
Rowley <Charlotte.Rowley@ons.gov.uk>
2025-02-27 11:19:32 +0000

    read data

---
+++ mapping/prep_data.R
@@ -0,0 +1,16 @@
+library(magrittr)
+
+get_df_from_db <- function() {
+    df <- read.delim(file = "data/median_annual_pay.tsv", na.strings = "")
+    
+    df <- dplyr::select(df, date, local.authority..district...unitary..as.of.April.2023., value) |>
+        dplyr::rename(year = date, lad = local.authority..district...unitary..as.of.April.2023., median_ann_pay = value) |>
+        dplyr::filter(!grepl("[^0-9]", median_ann_pay)) |>
+        data.frame()
+    
+    df$median_ann_pay < as.numeric(df$median_ann_pay)
+    
+    return(df)
+    
+}
+    
